eb0ace454083e6c86bf3d58d01b73c8933a13c28
patolino3A2024 <00001233600035SP@al.educacao.sp.gov.br>
2024-11-07 10:04:28 -0300

    geografia

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'qual e a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Austrália?',
+    'A capital da Austrália é Canberra'
+)
+
+criaCartao(
+    'Geografia',
+    'Em qual continente ficar o Egito?',
+    'Continente Africano'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual país com a maior população do mundo?',
+    'Índia'
+)
